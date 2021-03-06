bdbaf2849620afc4344ec0276b4d5363ac361fa7
Max Rydahl Andersen <manderse@redhat.com>
2021-03-06 23:49:34 +0100

    dfdf

--- help.sh
+++ help.sh
@@ -1,9 +1,6 @@
-echo "Running a jbang command"
+
 groupdir=$(echo "$GROUP" | sed "s/\./\//g")
 mkdir -p ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/
 curl -Ls https://sh.jbang.dev | bash -s - export -O ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.jar hello.java
 sh install.sh > ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom
 sh install.sh > ./pom.xml
-find . 
-#find ~/.m2
-env
