11c8f07e5e19a65c5bb55dae136270359e2e919a
Max Rydahl Andersen <manderse@redhat.com>
2021-03-06 23:34:47 +0100

    dfdffd

--- help.sh
+++ help.sh
@@ -3,7 +3,7 @@ groupdir=$(echo "$GROUP" | sed "s/\./\//g")
 mkdir -p ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/
 curl -Ls https://sh.jbang.dev | bash -s - export -O ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.jar hello.java
 sh install.sh > ~/.m2/repository/$groupdir/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom
-#sh install.sh > ./pom.xml
+sh install.sh > ./pom.xml
 find . 
 #find ~/.m2
 env
