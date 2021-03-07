e73b9c184db0c99f801a5e70b28d1aa8cfc67390
Max Rydahl Andersen <manderse@redhat.com>
2021-03-07 11:42:54 +0100

    use figlet

--- hello.java
+++ hello.java
@@ -1,11 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-// //DEPS <dependency1> <dependency2>
+//DEPS com.github.lalyos:jfiglet:0.0.8
 
-import static java.lang.System.*;
+import com.github.lalyos.jfiglet.FigletFont;
 
-public class hello {
+class hello {
 
-    public static void main(String... args) {
-        out.println("Hello World");
+    public static void main(String... args) throws Exception {
+        System.out.println(FigletFont.convertOneLine(
+               "Hello " + ((args.length>0)?args[0]:"jbang")));
     }
 }
